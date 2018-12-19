74ce7f1b5a610555fc5b0c3f2a08c991688cef9e
Jurre Thiel <xs2jrr@xs4all.nl>
2018-12-19 14:05:20 +0100

    Add option to run Dask tast singlethreaded in pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,25 @@ import dask.distributed
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        '--singlethread', action='store_true', dest='singlethread')
+
+
 @pytest.fixture(scope='session')
-def dask_client():
+def dask_client(request):
     # Start one Dask session for all tests, and close it on shutdown
     # This is more efficient than starting a Client every time a DynamicGame
     # is instantiated.
-    client = dask.distributed.Client()
+
+    if request.config.getoption('singlethread'):
+        # Run on a single thread
+        cluster = dask.distributed.LocalCluster(
+            n_workers=1, processes=False, threads_per_worker=1)
+    else:
+        # Run on the default local cluster
+        cluster = dask.distributed.LocalCluster()
+
+    client = dask.distributed.Client(cluster)
     yield client
     client.close()

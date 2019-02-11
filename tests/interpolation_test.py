031430fc264198d4852787d5f9ce2d3935c331f0
Jurre Thiel <xs2jrr@xs4all.nl>
2019-02-11 14:12:29 +0100

    In interpolation tests, run Chebyshev derivative tests on all examples.
    During development of the Hessian for Chebyshev interpolators, it turned
    out that some 2-D interpolation examples and 3-D interpolation examples
    are quite different. Therefore it is important to include both.

--- tests/interpolation_test.py
+++ tests/interpolation_test.py
@@ -284,10 +284,7 @@ def test_chebyshev_fit(nodes_per_state, degree, node_min, node_max, func):
 
 @pytest.mark.parametrize(
     'nodes_per_state, degree, node_min, node_max, func',
-    # Only use one test case to speed up testing. The correctness
-    # of the derivative should not depend on the particular function used,
-    # so this should not have any downsides
-    [interpolation_tests[2]]
+    interpolation_tests
 )
 def test_chebyshev_derivative(
     nodes_per_state, degree, node_min, node_max, func

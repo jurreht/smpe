294649904e930bb55d0ae0fde0608fc8cea0af6f
Jurre Thiel <3878745+jurreht@users.noreply.github.com>
2020-05-05 14:26:48 +0200

    Make sure actions in compute_next_state() is an AbstractVector.
    In the contraction mapping, it would be called provided as a Tuple,
    causing problems in dispatch.

--- src/SMPE.jl
+++ src/SMPE.jl
@@ -850,7 +850,7 @@ function calculate_value_function(
     end
     next_state_grid = let game=game, player_ind=player_ind
         pmap(
-            zipped -> compute_next_state(game, zipped[1], player_ind, zipped[2:end][player_ind], zipped[2:end]),
+            zipped -> compute_next_state(game, zipped[1], player_ind, zipped[2:end][player_ind], collect(zipped[2:end])),
             pool,
             zip(relevant_states, actions_grid...)
         )
